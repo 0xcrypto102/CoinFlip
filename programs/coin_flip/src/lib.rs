f0d59f88d51f9632b86f0001cd8d304e70f8436d
0xkartos <tiensucha5@gmail.com>
2024-04-02 22:51:52 +0300

    Update lib.rs

--- programs/coin_flip/src/lib.rs
+++ programs/coin_flip/src/lib.rs
@@ -7,7 +7,7 @@ mod constants;
 use anchor_lang::prelude::*;
 use instructions::*;
 
-declare_id!("HGtvZvwo9zBjG8Du7kpLdacEV9LongsNGS2bddcyCB1L");
+declare_id!("4gCjdyt4nGm61fZaiLjApNQyE8fA7rKag2LxNWULFMPC");
 
 #[program]
 pub mod coin_flip {
@@ -15,6 +15,14 @@ pub mod coin_flip {
 
     pub fn initialize(ctx: Context<Initialize>, fee: u64, max_bet: u64) -> Result<()> {
         instructions::initialize(ctx, fee, max_bet)
+    }
+
+    pub fn update_fee(ctx: Context<UpdateFee>, fee: u64) -> Result<()> {
+        instructions::update_fee(ctx, fee)
+    }
+
+    pub fn update_owner(ctx: Context<UpdateOwner>, new_owner: Pubkey) -> Result<()> {
+        instructions::update_owner(ctx, new_owner)
     }
 
     pub fn coin_flip_bet(ctx: Context<CoinFlipBet>,force: [u8; 32], guess: u8, amount: u64) -> Result<()> {
